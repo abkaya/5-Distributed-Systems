f8ddde336ab5710f813e0f58427db980ab6d8ffb
Abdil Kaya <abdil.kaya@student.uantwerpen.be>
2016-11-15 12:42:00 +0100

    Replication interface RMI method implementations  1/wip

--- src/be/uantwerpen/group1/systemy/node/Node.java
+++ src/be/uantwerpen/group1/systemy/node/Node.java
@@ -1,30 +1,81 @@
 package be.uantwerpen.group1.systemy.node;
 
 import java.rmi.RemoteException;
+import java.util.List;
 import java.util.logging.Level;
-
 import be.uantwerpen.group1.systemy.logging.SystemyLogger;
 import be.uantwerpen.group1.systemy.nameserver.NameServerInterface;
 import be.uantwerpen.group1.systemy.networking.RMI;
 import be.uantwerpen.group1.systemy.networking.TCP;
 import be.uantwerpen.group1.systemy.networking.MulticastSender;
 
-public class Node {
+public class Node implements NodeInterface
+{
+	List<String> ownedFiles;
+	List<String> localFiles;
+	List<String> downloadedFiles;
+	
+	/**
+	 * RMI method used by other nodes to check whether or not this node already knows it owns a file
+	 */
+	@Override
+	public boolean hasOwnedFile(String fileName) throws RemoteException
+	{
+		for(String files : ownedFiles)
+		{
+			if(files == fileName)
+				return true;
+		}
+		return false;
+	}
 
+	/**
+	 * RMI method used by other nodes to check whether or not this node has a file available locally
+	 */
+	@Override
+	public boolean hasLocalFile(String fileName) throws RemoteException
+	{
+		for(String files : ownedFiles)
+		{
+			if(files == fileName)
+				return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public void addOwnedFile(String fileName) throws RemoteException
+	{
+		ownedFiles.add(fileName);
+	}
+
+	@Override
+	public void addLocalFile(String fileName) throws RemoteException
+	{
+		localFiles.add(fileName);
+	}
+	
 	private static String logName = Node.class.getName() + " >> ";
 
-	public static void main(String args[]) throws RemoteException {
-
+	public static void main(String args[]) throws RemoteException
+	{
+		
+		/*
+		 * Concerning the Files this node owns and the replication of these files: How to keep a list of files. Keep two lists? Files owned
+		 * and filesHeld. Remove files?
+		 */
+		
 		NameServerInterface nsi = null;
 		String remoteNSName = "NameServerInterface";
-		/* this is our IP, we now assume not to have the DNS IP, which we'll receive after retransmission
-		 * by the DNS server over a TCP socket.
-		*/
+		/*
+		 * this is our IP, we now assume not to have the DNS IP, which we'll receive after retransmission by the DNS server over a TCP
+		 * socket.
+		 */
 		String host = "192.168.1.103";
 		String dnsIP = null;
 		String hostnameIP = "Node1,192.168.1.103";
 
-		/*Don't mind the awful port names. It's just to get everyone acquainted with them*/
+		/* Don't mind the awful port names. It's just to get everyone acquainted with them */
 		int dnsPort = 1099;
 		int multicastPort = 2000;
 		int tcpFileTranferPort = 2001;
@@ -34,17 +85,15 @@ public class Node {
 		/*
 		 * Assessing one's IP address can become tricky when multiple network interfaces are involved. For instance, I'm getting the APIPA
 		 * address 169.254.202.83, which is undesirable. We could work this out in the future, but let's use the manually determined IP
-		 * address for now - abdil
-		 * 
+		 * address for now
 		 * String myIP = InetAddress.getLocalHost().getHostAddress();
 		 */
 		MulticastSender.send("234.0.113.0", multicastPort, hostnameIP);
 
 		/*
-		 * Now we imagine we don't have a clue what the DNS IP is, and hope for TCP retransmission
-		 * to get ahold of the DNS server's IP. We'll await for the DNS server to get back at us
-		 * and continue with RMI once we get it.
-		*/
+		 * Now we imagine we don't have a clue what the DNS IP is, and hope for TCP retransmission to get ahold of the DNS server's IP.
+		 * We'll await for the DNS server to get back at us and continue with RMI once we get it.
+		 */
 		TCP dnsIPReceiver = new TCP(host, tcpDNSRetransmissionPort);
 		dnsIP = dnsIPReceiver.receiveText();
 
@@ -54,20 +103,21 @@ public class Node {
 		// test to see whether our RMI class does its job properly. Spoiler alert: it does.
 		SystemyLogger.log(Level.INFO, logName + "DNS RMI IP address request for machine hosting file: 'HQImage.jpg' \n "
 				+ "DNS Server RMI tree map return : " + nsi.getIPAddress(requestedFile));
-		//System.out.println("DNS RMI IP address request for machine hosting file: 'HQImage.jpg' \n "
-		//		+ "DNS Server RMI tree map return : " + nsi.getIPAddress(requestedFile));
+		// System.out.println("DNS RMI IP address request for machine hosting file: 'HQImage.jpg' \n "
+		// + "DNS Server RMI tree map return : " + nsi.getIPAddress(requestedFile));
 
-		//Temporarily using the same node as if it were some other node hosting files
+		// Temporarily using the same node as if it were some other node hosting files
 
 		TCP fileServer = new TCP(host, tcpFileTranferPort);
-		new Thread(() -> {
+		new Thread(() ->
+		{
 			fileServer.listenToSendFile();
 		}).start();
 
-		//request the file from the server hosting it, according to the dns server
+		// request the file from the server hosting it, according to the dns server
 		TCP fileClient = new TCP(tcpFileTranferPort, nsi.getIPAddress(requestedFile));
 		fileClient.receiveFile(requestedFile);
-		//As simple as that!
+		// As simple as that!
 
 	}
 
--- src/be/uantwerpen/group1/systemy/node/ReplicationInterface.java
+++ src/be/uantwerpen/group1/systemy/node/ReplicationInterface.java
@@ -3,6 +3,7 @@ package be.uantwerpen.group1.systemy.node;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface ReplicationInterface extends Remote {
-    String getIPAddress (String fileName) throws RemoteException;
+public interface ReplicationInterface extends Remote
+{
+	String getIPAddress(String fileName) throws RemoteException;
 }
